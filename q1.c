606b0500bafe75bbcd61aec1a003b4c2af8ffdf4
Hemen Lahkar <147333013+hemenlahkar@users.noreply.github.com>
2024-10-23 15:40:10 +0530

    Create q1.c
    Added program for the first flowchart

---
+++ q1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int a, b, c;
+    printf("Enter the two number: ");
+    scanf("%d%d", &a, &b);
+    c = a + b;
+    printf("\n%d + %d = %d", a, b, c);
+    return 0;
+}
