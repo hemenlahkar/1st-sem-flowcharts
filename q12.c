c77263e9ef9fc70af499443a75faa120abb71177
Hemen Lahkar <hemenlahkar3108+github@gmail.com>
2024-10-28 13:33:15 +0530

    12th flowchart added

---
+++ q12.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int A, B;
+    printf("\nEnter two numbers: ");
+    scanf("%d%d",&A, &B);
+    A<B?printf("\n%d is smaller", A):printf("\n%d is smaller", B);
+
+    return 0;
+}
