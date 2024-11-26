4074016ce9347df6b253ba0c99ac39b94230f5a7
Hemen Lahkar <hemenlahkar3108+github@gmail.com>
2024-11-26 21:16:20 +0530

    some imrovements

--- q67.c
+++ q67.c
@@ -6,14 +6,11 @@ void displayPascal(int n)
     for (int i = 1; i <= n; i++)
     {
         for (int j = 1; j <= n - i; j++)
-            printf(" ");
+            printf("  ");
         int coef = 1;
         for (int k = 1; k <= i; k++)
         {
-            if (k == 1 && i != 1)
-                printf(" %d", coef);
-            else
-                printf("%3d", coef);
+            printf("%4d", coef);
             coef = coef * (i - k) / k;
         }
         printf("\n");
